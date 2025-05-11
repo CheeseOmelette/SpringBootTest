7ab03fea1d23e28bfe5a6c30c5d6de80a93afb06
CheeseOmelette <poketvgameplays@gmail.com>
2025-05-11 19:12:42 -0300

    First Commit

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Welcome.java
@@ -0,0 +1,15 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome!!!";
+	}
+	
+
+}
